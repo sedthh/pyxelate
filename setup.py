485d2e999ed0068ebbb2162e3acc7896a422b521
Dávid Komorowicz <dawars00@gmail.com>
2020-02-29 13:11:45 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pyxelate',
+      version='1.0',
+      description='Pyxelate is a Python class that converts images into tiny pixel arts with limited color palettes.',
+      url='http://github.com/sedthh/pyxelate',
+      author='sedthh',
+      license='MIT',
+      packages=[''],
+      zip_safe=False,
+      install_requires=[
+          'scikit-image==0.16.2', 'scikit-learn==0.22.1'
+      ],
+      )
