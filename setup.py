b99ca2429181dc34b81923c983bb9333a739b8fe
Matt Welsh <mdw@mdw.la>
2021-12-07 21:25:09 -0800

    Adding 'pyxelate' command to setup.py.

--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@
 
 from setuptools import setup
 
-setup(name='pyxelate',
-      version='2.0.3',
-      description='Pyxelate is a Python class that converts images into 8-bit pixel art.',
-      url='http://github.com/sedthh/pyxelate',
-      author='sedthh',
-      license='MIT',
-      packages=['pyxelate'],
-      zip_safe=False,
-      install_requires=[
-          'scikit-learn>=0.24.1', 'scikit-image>=0.18.1','numba>=0.53.1'
-      ],
-      )
+setup(
+    name="pyxelate",
+    version="2.0.3",
+    description="Pyxelate is a Python class that converts images into 8-bit pixel art.",
+    url="http://github.com/sedthh/pyxelate",
+    author="sedthh",
+    license="MIT",
+    packages=["pyxelate"],
+    zip_safe=False,
+    install_requires=["scikit-learn>=0.24.1", "scikit-image>=0.18.1", "numba>=0.53.1"],
+    entry_points={
+        "console_scripts": [
+            "pyxelate = pyxelate.main:main",
+        ],
+    },
+)
